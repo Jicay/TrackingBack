57d5b590585bad152c5e9592232392fed977bef4
jcsabourin <jeanclement.sabourin@gmail.com>
2019-10-12 20:42:48 +0200

    Test

--- src/main/java/fr/jicay/tracking/web/SessionController.java
+++ src/main/java/fr/jicay/tracking/web/SessionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("sessions")
 public class SessionController {
